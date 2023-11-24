09c12905525382fe27349be2a16de8726bb7f67b
Samuel Lucas <samuellucas24383@gmail.com>
2023-11-24 04:13:23 -0300

    :ok_hand: feat: New refactor code

--- src/routes/swaggerRouter.ts
+++ src/routes/swaggerRouter.ts
@@ -1,11 +1,9 @@
-import fs from 'fs';
 import express from "express";
 import swaggerUi from 'swagger-ui-express';
 
 import swaggerJson from '../helps/swagger.json';
 
 const swaggerRouter = express.Router();
-
 
 swaggerRouter.use('/', swaggerUi.serve);
 
